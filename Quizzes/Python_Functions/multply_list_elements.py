537f49bbeed3703f9464673cdccf5f0418339436
Timothy-Wangwe <wangwetymothy@gmail.com>
2020-07-03 19:48:04 +0300

    Done with the assignment

---
+++ Quizzes/Python_Functions/multply_list_elements.py
@@ -0,0 +1,11 @@
+lst = [2, 4, 5, 10]
+mult = 1
+
+for el in lst:
+	mult = el * mult
+
+print(f'Sum: {mult}')
+
+# mult is initialized/populated with one(1) 
+# because muiltiplying 1 with any of the
+# elements doesn't affect the values
