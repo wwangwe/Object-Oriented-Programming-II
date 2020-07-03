a21b9174ed80325bd4ac8d0ec64a93672a5f7a39
Timothy-Wangwe <wangwetymothy@gmail.com>
2020-07-03 20:31:58 +0300

    Updated docs

--- Quizzes/Python_Functions/multply_list_elements.py
+++ Quizzes/Python_Functions/multply_list_elements.py
@@ -1,17 +1,4 @@
-nums = [2, 4, 5, 10]
-mult = 1
-
-def incest(lst):
-	global mult
-
-	for el in lst:
-		mult = el * mult
-
-	print(f'Sum: {mult}')
-
-incest(nums)
-
-# mult is initialized/populated with one(1) 
-# because muiltiplying 1 with any of the
-# elements doesn't affect the values
-# Called *global* because mult is outside function
+def sayHello():
+	    print('Hello World!') 
+sayHello() 
+sayHello()
