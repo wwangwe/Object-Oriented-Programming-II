26005d6245dd0a0fd9f37469cd6cfad9ae6bda4f
Timothy-Wangwe <wangwetymothy@gmail.com>
2020-07-06 07:22:45 +0300

    This is a Panic Commit

---
+++ 5.Python_Modules/AutoGit/auto.py
@@ -0,0 +1,9 @@
+from os import system
+
+
+def git_panic(change = '*', message = 'This is a Panic Commit'):
+    system('git pull')
+    system(f'git add {change}')
+    system(f'git commit -m "{message}"')
+    # system('git push')
+    system('git status')
